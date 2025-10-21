44af6062f26e72d9a46795c8b19c0386f2c65339
Ayush Khaire <codeeayush@gmail.com>
2025-10-21 22:18:37 +0530

    code for loading embeding models

---
+++ RAG/pre_processor.py
@@ -0,0 +1,15 @@
+from langchain.text_splitter import CharacterTextSplitter
+from config.logger_config import logger
+
+class Chunker:
+    def __init__(self):
+        self.text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len,
+        )
+
+    def chunk_text(self, text: str):
+        chunks = self.text_splitter.split_text(text)
+        return chunks
